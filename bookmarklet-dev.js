558a27c99f5b3e4552cc13662ae2d6aa0b72779c
Raghav Sood <raghavsood@appaholics.in>
2017-01-11 22:14:18 +0800

    Start over

--- bookmarklet-dev.js
+++ bookmarklet-dev.js
@@ -1,1 +1,1 @@
-(function(){var a=document.createElement("script");a.src="https://rawgithub.com/Zirak/SO-ChatBot/master/master.js";a.onload=function(){bot.activateDevMode();};document.head.appendChild(a)})();
+(function(){var a=document.createElement("script");a.src="https://rawgithub.com/room-15/SO-ChatBot/master/master.js";a.onload=function(){bot.activateDevMode();};document.head.appendChild(a)})();
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-(function(){var a=document.createElement("script");a.src="https://rawgit.com/Zirak/SO-ChatBot/master/master.js",document.head.appendChild(a)})()
+(function(){var a=document.createElement("script");a.src="https://rawgit.com/room-15/SO-ChatBot/master/master.js",document.head.appendChild(a)})()
